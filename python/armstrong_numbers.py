e57a5e84b8121ca9fc35d0ba6f25cc90d36cfdd6
Jeremiah Mauga <miahmauga@gmail.com>
2021-05-28 07:12:36 -0700

    Jeremiah's python attempt at armstrong_numbers

--- python/armstrong_numbers.py
+++ python/armstrong_numbers.py
@@ -1,2 +1,30 @@
 # How can you make this more scalable and reusable later?
-def find_armstrong_numbers(numbers):
+def find_armstrong_numbers(list):
+    
+    armstrong_list = []
+
+
+    for i in range(len(list)) :
+        string = str(i)
+        number_list = list(string)
+        number_list = []
+
+        for x in number_list :
+            number_list.append(int(x))
+
+        sum = 0    
+
+        for y in number_list :
+            sum += y ** len(number_list)
+
+        if i == sum :
+            armstrong_list.append(i) 
+
+    print(armstrong_list)
+
+    return armstrong_list
+
+
+find_armstrong_numbers([0, 1, 2, 3, 4, 5, 6, 7])
+
+
--- python/armstrong_numbers_spec.py
+++ python/armstrong_numbers_spec.py
@@ -1,7 +1,29 @@
 # The code is written as driver code. Can you convert it to Unit Test?
+import unittest
+
 from armstrong_numbers import find_armstrong_numbers
 
-print(find_armstrong_numbers([0]) == [0]) 
-print(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
-print(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
-print(find_armstrong_numbers(list(range(0,999))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+class TestArmstrong(unittest.TestCase) :
+    
+    
+    "Testing for 0"
+    def test_armstrong_0(self) :
+        self.assertEqual(find_armstrong_numbers([0]) == [0]) 
+    
+    "Testing for 0 - 8"
+    def test_armstrong_8(self) :
+        self.assertEqual(find_armstrong_numbers(list(range(0, 8))) == [0, 1, 2, 3, 4, 5, 6, 7])
+
+    "Testing for 0 - 100"
+    def test_armstrong_100(self) :
+        self.assertEqual(find_armstrong_numbers(list(range(0,100))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+
+    "Testing for 0 - 900"
+    def test_armstrong_900(self) :
+        self.assertEqual(find_armstrong_numbers(list(range(0,900))) == [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 153, 370, 371, 407])
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
